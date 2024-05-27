148514fcaf7fbbce78d2b3743af0f99ff74958c2
Xaver <xaver@everpress.co>
2024-05-27 15:26:30 +0200

    updated readme

--- mailster-multi-smtp.php
+++ mailster-multi-smtp.php
@@ -1,6 +1,7 @@
 <?php
 /*
 Plugin Name: Mailster Multi SMTP
+Requires Plugins: mailster
 Plugin URI: https://mailster.co/?utm_campaign=wporg&utm_source=wordpress.org&utm_medium=plugin&utm_term=Multi+SMTP
 Description: Allows to use multiple SMTP connection for the Mailster Newsletter Plugin
 Version: 1.3.1
@@ -15,6 +16,5 @@ define( 'MAILSTER_MULTISMTP_VERSION', '1.3.1' );
 define( 'MAILSTER_MULTISMTP_REQUIRED_VERSION', '2.2' );
 define( 'MAILSTER_MULTISMTP_FILE', __FILE__ );
 
-require_once dirname( __FILE__ ) . '/classes/multismtp.class.php';
+require_once __DIR__ . '/classes/multismtp.class.php';
 new MailsterMultiSMTP();
-
